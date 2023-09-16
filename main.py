e728808a73f969bdced5938762f7660066e2751a
Samuel Theofie <55137379+BlazedEngima@users.noreply.github.com>
2023-09-16 14:17:13 +0700

    Added Employee class and init function
    Separating to classes and functions increases readability

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from tkinter import *
+import tkinter as tk
+from datetime import date
 
-if __name__ == '__main__':
-    window = Tk()
+class EmployeeData:
+    def __init__(self, szId = '', szName = '', dtmBirthday = date.min):
+        self.szId = szId
+        self.szName = szName
+        self.dtmBirthday = dtmBirthday
+
+    
+
+def win_initialize():
+    window = tk.Tk()
     window.geometry("720x360")
     window.title("Main")
 
+    curr_date = tk.Label(window, text=date.today(), font=('Arial Bold', 10))
+    curr_date.pack(padx=10, pady=10, anchor='w')
+
+    return window
+
+if __name__ == '__main__':
+    window = win_initialize()
     window.mainloop()
