bba55da38bd85be5a6296e91d78f16c86e9c9df5
Samuel Theofie <55137379+BlazedEngima@users.noreply.github.com>
2023-09-16 15:52:08 +0700

    Added buttons in UI
    Button navigation to view different data entries (To be Implemented)

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from datetime import date
 
-rows, cols = 5, 5
-
 class MainForm:
-    def __init__(self, employeeList = [], uiList = [None] * 5):
+    def __init__(self, window, employeeList = [], uiList = [None] * 5):
         self.dtmToday = date.today()
         self.employeeList = employeeList
         self.uiList = uiList
+        self.window = window
+        self.headers = ['NO', 'ID', 'NAME', 'BIRTHDAY', 'AGE']
+        self.width = [10, 15, 25, 20, 30]
+        self.button_icon = ['|<', '<<', '<', '>', '>>', '>|']
     
     def add_employee(self, employeeData):
         self.employeeList.append(employeeData)
@@ -18,6 +20,42 @@ class MainForm:
     def remove_ui(self, index):
         self.uiList[index] = None
 
+    def win_initialize(self):
+        self.window.geometry("840x240")
+        self.window.title("Main")
+
+        curr_date = tk.Label(self.window, text=date.today(), font=('Arial Bold', 10))
+        curr_date.pack(padx=10, pady=10, anchor='w')
+        
+    def initialize_ui(self):
+        self.__init_tableFrame__()
+        self.__init_buttonFrame__()
+
+    def __init_buttonFrame__(self):
+        buttonFrame = tk.Frame(self.window)
+
+        for i in range(6):
+            button = tk.Button(buttonFrame, text=self.button_icon[i], width=5, font=('Arial bold', 10))
+            button.grid(row=0, column=i)
+        
+        buttonFrame.pack(fill='x', padx=10, pady=20)
+
+    def __init_tableFrame__(self):
+        rows, cols = 5, 5
+        tableFrame = tk.Frame(self.window)
+        
+        for col, header in enumerate(self.headers):
+            label = tk.Label(tableFrame, text=header, borderwidth=1, width=self.width[col], relief="solid", font=('Arial bold', 10))
+            label.grid(row=0, column=col)
+        
+        for i in range(rows):
+            for j in range(cols):
+                entry = tk.Label(tableFrame, text=test_data[i][j], borderwidth=1, width=self.width[j], relief="solid", font=('Arial', 10))
+                entry.grid(row=i + 1, column=j, sticky="nsew")
+        
+        tableFrame.pack(fill='x', padx=10)
+
+    #Test function
     def display(self):
         print(self.employeeList)
         print(self.uiList)
@@ -36,30 +74,6 @@ class UIData:
         self.lblBirthday = lblBirthday
         self.lblAge = lblAge
 
-def win_initialize(window):
-    window.geometry("840x360")
-    window.title("Main")
-
-    curr_date = tk.Label(window, text=date.today(), font=('Arial Bold', 10))
-    curr_date.pack(padx=10, pady=10, anchor='w')
-
-def initialize_ui(window):
-    headers = ['NO', 'ID', 'NAME', 'BIRTHDAY', 'AGE']
-    width = [10, 15, 25, 20, 30]
-
-    tableFrame = tk.Frame(window)
-    
-    for col, header in enumerate(headers):
-        label = tk.Label(tableFrame, text=header, borderwidth=1, width=width[col], relief="solid", font=('Arial bold', 10))
-        label.grid(row=0, column=col)
-    
-    for i in range(rows):
-        for j in range(cols):
-            entry = tk.Label(tableFrame, text=test_data[i][j], borderwidth=1, width=width[j], relief="solid", font=('Arial', 10))
-            entry.grid(row=i + 1, column=j, sticky="nsew")
-    
-    tableFrame.pack(fill='x', padx=10)
-
 
 if __name__ == '__main__':
     # test = MainForm()
@@ -72,7 +86,10 @@ if __name__ == '__main__':
        (5,'Shubham','Delhi',21, 30)]
 
 
-    window = tk.Tk()
-    win_initialize(window)
-    initialize_ui(window)
-    window.mainloop()
+    root = tk.Tk()
+
+    window = MainForm(root)
+    window.win_initialize()
+    window.initialize_ui()
+
+    root.mainloop()
